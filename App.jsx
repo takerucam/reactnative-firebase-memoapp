b79dfcb3192c2cbe9be844945091aed784219615
Takeru Miyake <m18kecam777@gmail.com>
2021-12-20 00:10:37 +0900

    install eslint

--- App.jsx
+++ App.jsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// eslint-disable-next-line react/function-component-definition
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
     </View>
   );
