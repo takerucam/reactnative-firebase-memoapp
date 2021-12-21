fa9f57a95da15aa947528a1745f22f47830e0af7
Takeru Miyake <m18kecam777@gmail.com>
2021-12-21 12:17:44 +0900

    Extract Components

--- App.jsx
+++ App.jsx
@@ -1,38 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import AppBar from './src/components/appBar';
+import CircleButton from './src/components/circleButton';
+import MemoList from './src/components/memoList';
 
 // eslint-disable-next-line react/function-component-definition
 export default function App() {
   return (
     <View style={styles.container}>
 
-      {/* Header */}
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>Memo App</Text>
-          <Text style={styles.appbarRight}>ログアウト</Text>
-        </View>
-      </View>
-
-      {/* memoList */}
-      <View>
-        {/* memoListItem */}
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-      </View>
-
-      {/* memoAddButton */}
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabel}>+</Text>
-      </View>
+      <AppBar />
+      <MemoList />
+      <CircleButton>+</CircleButton>
 
     </View>
   );
@@ -42,67 +21,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-  appbar: {
-    width: '100%',
-    height: 104,
-    backgroundColor: '#467FD3',
-    justifyContent: 'flex-end',
-  },
-  appbarInner: {
-    alignItems: 'center',
-  },
-  appbarRight: {
-    position: 'absolute',
-    right: 19,
-    bottom: 16,
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-  appbarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    color: '#FFFFFF',
-    fontWeight: 'bold',
-  },
-  memoListItem: {
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.15)',
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484',
-  },
-  circleButton: {
-    backgroundColor: '#467FD3',
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    elevation: 8,
-  },
-  circleButtonLabel: {
-    color: 'white',
-    fontSize: 40,
-    lineHeight: 40,
   },
 });
---
+++ src/components/memoList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function memoList() {
+  return (
+    <View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  memoListItem: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+});
